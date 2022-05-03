ed23bdf9abe7eb4d576d9efe4f972e2ddba39f60
FlutLab.io <>
2022-05-03 14:33:18 +0000

    Switching to branch master2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:urbina/material.dart'
 
 void main() {
   runApp(MascotasApp());
@@ -11,58 +12,9 @@ class MascotasApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'App Veterinaria',
         theme: ThemeData(
-          primarySwatch: Colors.green,
+          primarySwatch: Colors.cyan,
         ),
         home: PaginaInicial());
   } // Widget
 } // MascotasApp widget sin estado
 
-class PaginaInicial extends StatefulWidget {
-  PaginaInicial({Key? key}) : super(key: key);
-  @override
-  _PaginaInicialState createState() => _PaginaInicialState();
-} // Pagina con Estado
-
-class _PaginaInicialState extends State<PaginaInicial> {
-  List<String> images = [
-    "assets/images/scenary.jpg",
-    "assets/images/scenary_red.jpg",
-    "assets/images/waterfall.jpg",
-    "assets/images/tree.jpg",
-    "assets/images/scenery.jpg",
-    "assets/images/scenary_red.jpg",
-    "assets/images/waterfall.jpg",
-    "assets/images/tree.jpg",
-    "assets/images/scenary.jpg",
-    "assets/images/scenary_red.jpg",
-    "assets/images/waterfall.jpg",
-    "assets/images/tree.jpg",
-    "assets/images/scenery.jpg",
-    "assets/images/scenary_red.jpg",
-    "assets/images/waterfall.jpg",
-    "assets/images/tree.jpg",
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter GridView"),
-        ),
-        body: GridView.custom(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          childrenDelegate: SliverChildBuilderDelegate(
-            (BuildContext, index) {
-              return Image.asset(
-                images[index],
-                fit: BoxFit.cover,
-              );
-            },
-            childCount: 16,
-          ),
-          padding: EdgeInsets.all(10),
-          shrinkWrap: true,
-        ));
-  }
-}
